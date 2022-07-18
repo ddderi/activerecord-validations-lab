2735d5be440b453298df2d5ead440426c2de7afc
ddderi <maxime830@live.fr>
2022-07-18 05:31:25 +0200

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ActiveRecord::Base
+validates :title, presence: true
+validates :content, length: {minimum: 250}
+validates :summary, length: {maximum: 250}
+validates :category, inclusion: {in: %w(fiction non-fiction)}
+validate :is_clickbait?
+  
+    CLICKBAIT_PATTERNS = [
+      /Won't Believe/,
+      /Secret/,
+      /Top [0-9]*/,
+      /Guess/
+    ]
+  
+    def is_clickbait?
+      if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+        errors.add(:title, "must be clickbait")
+      end
+    end
+
+
 end
